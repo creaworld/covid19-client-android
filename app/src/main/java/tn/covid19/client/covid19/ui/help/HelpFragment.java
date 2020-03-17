ab11ca11662b22b20058c7b5a31aae31647321fa
Dali Abdallah <dali.abdallah@door2door.io>
2020-03-18 00:37:07 +0100

    Add webview with the default website

--- app/src/main/java/tn/covid19/client/covid19/ui/help/HelpFragment.java
+++ app/src/main/java/tn/covid19/client/covid19/ui/help/HelpFragment.java
@@ -1,61 +1,45 @@
 package tn.covid19.client.covid19.ui.help;
 
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import tn.covid19.client.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HelpFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HelpFragment extends Fragment {
-  // TODO: Rename parameter arguments, choose names that match
-  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-  private static final String ARG_PARAM1 = "param1";
-  private static final String ARG_PARAM2 = "param2";
 
-  // TODO: Rename and change types of parameters
-  private String mParam1;
-  private String mParam2;
+  WebView mWebView ;
 
   public HelpFragment() {
     // Required empty public constructor
   }
 
-  /**
-   * Use this factory method to create a new instance of
-   * this fragment using the provided parameters.
-   *
-   * @param param1 Parameter 1.
-   * @param param2 Parameter 2.
-   * @return A new instance of fragment HelpFragment.
-   */
-  // TODO: Rename and change types and number of parameters
-  public static HelpFragment newInstance(String param1, String param2) {
-    HelpFragment fragment = new HelpFragment();
-    Bundle args = new Bundle();
-    args.putString(ARG_PARAM1, param1);
-    args.putString(ARG_PARAM2, param2);
-    fragment.setArguments(args);
-    return fragment;
-  }
-
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    if (getArguments() != null) {
-      mParam1 = getArguments().getString(ARG_PARAM1);
-      mParam2 = getArguments().getString(ARG_PARAM2);
-    }
   }
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    return inflater.inflate(R.layout.fragment_help, container, false);
+    View root = inflater.inflate(R.layout.fragment_help, container, false);
+    mWebView = root.findViewById(R.id.webView);
+    mWebView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+    mWebView.setScrollbarFadingEnabled(true);
+    WebSettings webViewSettings = mWebView.getSettings();
+    webViewSettings.setJavaScriptEnabled(true);
+    webViewSettings.setUseWideViewPort(true);
+    webViewSettings.setBuiltInZoomControls(true);
+    webViewSettings.setLoadWithOverviewMode(true);
+    webViewSettings.setDomStorageEnabled(true);
+    mWebView.setWebViewClient(new WebViewClient());
+    mWebView.setWebChromeClient(new WebChromeClient());
+    mWebView.loadUrl("http://covid-19.tn/");
+    return root;
   }
 }
