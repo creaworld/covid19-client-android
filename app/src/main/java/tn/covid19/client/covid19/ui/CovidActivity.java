38fbff293c98dac16b91eb9e85a1e4de91880604
shadyabidi <chadi.abidi@esprit.tn>
2020-03-17 15:22:59 +0100

    Add Fragments and polish home screen

--- app/src/main/java/tn/covid19/client/covid19/ui/CovidActivity.java
+++ app/src/main/java/tn/covid19/client/covid19/ui/CovidActivity.java
@@ -1,4 +1,4 @@
-package tn.covid19.client.legacy;
+package tn.covid19.client.covid19.ui;
 
 import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,7 +19,8 @@ public class CovidActivity extends AppCompatActivity {
     // Passing each menu ID as a set of Ids because each
     // menu should be considered as top level destinations.
     AppBarConfiguration appBarConfiguration =
-        new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications).build();
+        new AppBarConfiguration.Builder(R.id.navigation_home, R.id.navigation_dashboard,
+            R.id.navigation_notifications, R.id.navigation_visio, R.id.navigation_help).build();
     NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
     NavigationUI.setupWithNavController(navView, navController);
